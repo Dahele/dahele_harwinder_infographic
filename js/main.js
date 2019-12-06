4a137a468bcf9bd13fea9138c799a80cf9ab5a0b
Dahele <h_dahele@F1000-mac-07.fc.ca>
2019-12-05 21:22:17 -0500

    adding js

---
+++ js/main.js
@@ -0,0 +1,186 @@
+/* circle no.1 */
+TweenMax.to("#inside_1_", 0.001, {opacity:0});
+    function show(){
+        TweenMax.to("#inside_1_", 0.5, {opacity:1  ,delay:0});
+    }
+TweenMax.to("#outer_1_", 0.001, {opacity:0});
+    function show1(){
+        TweenMax.to("#outer_1_", 0.5, {opacity:1  ,delay:0.5});
+    }
+    function hide(){
+        TweenMax.to("#outer_1_", 0.5, {opacity:0  ,delay:1.5});
+    }
+TweenMax.to("#text_1", 0.001, {scale:0});
+    function show2(){
+        TweenMax.to("#text_1", 1, {scale:1, delay:0})
+    }
+
+
+/* circle no.2 */
+
+    TweenMax.to("#inside_2_", 0.001, {opacity:0});
+    function show3(){
+        TweenMax.to("#inside_2_", 0.5, {opacity:1  ,delay:0});
+    }
+TweenMax.to("#outer_2_", 0.001, {opacity:0});
+    function show4(){
+        TweenMax.to("#outer_2_", 0.5, {opacity:1  ,delay:0.5});
+    }
+    function hide1(){
+        TweenMax.to("#outer_2_", 0.5, {opacity:0  ,delay:1.5});
+    }
+TweenMax.to("#text_2", 0.001, {scale:0});
+    function show17(){
+        TweenMax.to("#text_2", 1, {scale:1, delay:0})
+    }
+
+/* circle no.3 */
+
+TweenMax.to("#inside_3_", 0.001, {opacity:0});
+function show5(){
+    TweenMax.to("#inside_3_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_3_", 0.001, {opacity:0});
+function show6(){
+    TweenMax.to("#outer_3_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide2(){
+    TweenMax.to("#outer_3_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_3", 0.001, {scale:0});
+function show18(){
+    TweenMax.to("#text_3", 1, {scale:1, delay:0})
+}
+
+/* circle no.4 */
+
+TweenMax.to("#inside_4_", 0.001, {opacity:0});
+function show7(){
+    TweenMax.to("#inside_4_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_4_", 0.001, {opacity:0});
+function show8(){
+    TweenMax.to("#outer_4_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide3(){
+    TweenMax.to("#outer_4_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_4", 0.001, {scale:0});
+function show19(){
+    TweenMax.to("#text_4", 1, {scale:1, delay:0})
+}
+
+
+/* circle no.5 */
+
+TweenMax.to("#inside_5_", 0.001, {opacity:0});
+function show9(){
+    TweenMax.to("#inside_5_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_5_", 0.001, {opacity:0});
+function show10(){
+    TweenMax.to("#outer_5_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide4(){
+    TweenMax.to("#outer_5_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_5", 0.001, {scale:0});
+function show20(){
+    TweenMax.to("#text_5", 1, {scale:1, delay:0})
+}
+
+/* circle no.6 */
+
+TweenMax.to("#inside_6_", 0.001, {opacity:0});
+function show11(){
+    TweenMax.to("#inside_6_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_6_", 0.001, {opacity:0});
+function show12(){
+    TweenMax.to("#outer_6_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide5(){
+    TweenMax.to("#outer_6_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_6", 0.001, {scale:0});
+function show21(){
+    TweenMax.to("#text_6", 1, {scale:1, delay:0})
+}
+
+/* circle no.7 */
+
+TweenMax.to("#inside_7_", 0.001, {opacity:0});
+function show13(){
+    TweenMax.to("#inside_7_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_7_", 0.001, {opacity:0});
+function show14(){
+    TweenMax.to("#outer_7_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide6(){
+    TweenMax.to("#outer_7_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_7", 0.001, {scale:0});
+function show22(){
+    TweenMax.to("#text_7", 1, {scale:1, delay:0})
+}
+
+
+/* circle no.8 */
+
+TweenMax.to("#inside_8_", 0.001, {opacity:0});
+function show15(){
+    TweenMax.to("#inside_8_", 0.5, {opacity:1  ,delay:0});
+}
+TweenMax.to("#outer_8_", 0.001, {opacity:0});
+function show16(){
+    TweenMax.to("#outer_8_", 0.5, {opacity:1  ,delay:0.5});
+}
+function hide7(){
+    TweenMax.to("#outer_8_", 0.5, {opacity:0  ,delay:1.5});
+}
+TweenMax.to("#text_8", 0.001, {scale:0});
+function show23(){
+    TweenMax.to("#text_8", 1, {scale:1, delay:0})
+}
+
+document.querySelector("#darkredCircle").addEventListener("mouseover", show)
+document.querySelector("#darkredCircle").addEventListener("mouseover", show1)
+document.querySelector("#darkredCircle").addEventListener("mouseover", show2)
+document.querySelector("#blackCircle").addEventListener("mouseover", show3)
+document.querySelector("#blackCircle").addEventListener("mouseover", show4)
+document.querySelector("#skyblueCircle").addEventListener("mouseover", show5)
+document.querySelector("#skyblueCircle").addEventListener("mouseover", show6)
+document.querySelector("#redCircle_2_").addEventListener("mouseover", show7)
+document.querySelector("#redCircle_2_").addEventListener("mouseover", show8)
+document.querySelector("#silverCircle").addEventListener("mouseover", show9)
+document.querySelector("#silverCircle").addEventListener("mouseover", show10)
+document.querySelector("#yellowCircle_1_").addEventListener("mouseover", show11)
+document.querySelector("#yellowCircle_1_").addEventListener("mouseover", show12)
+document.querySelector("#blueCircle_1_").addEventListener("mouseover", show13)
+document.querySelector("#blueCircle_1_").addEventListener("mouseover", show14)
+document.querySelector("#orangeCircle_1_").addEventListener("mouseover", show15)
+document.querySelector("#orangeCircle_1_").addEventListener("mouseover", show16)
+document.querySelector("#blackCircle").addEventListener("mouseover", show17)
+document.querySelector("#skyblueCircle").addEventListener("mouseover", show18)
+document.querySelector("#redCircle_2_").addEventListener("mouseover", show19)
+document.querySelector("#silverCircle").addEventListener("mouseover", show20)
+document.querySelector("#yellowCircle_1_").addEventListener("mouseover", show21)
+document.querySelector("#blueCircle_1_").addEventListener("mouseover", show22)
+document.querySelector("#orangeCircle_1_").addEventListener("mouseover", show23)
+document.querySelector("#outer_1_").addEventListener("mouseout", hide)
+document.querySelector("#outer_2_").addEventListener("mouseout", hide1)
+document.querySelector("#outer_3_").addEventListener("mouseout", hide2)
+document.querySelector("#outer_4_").addEventListener("mouseout", hide3)
+document.querySelector("#outer_5_").addEventListener("mouseout", hide4)
+document.querySelector("#outer_6_").addEventListener("mouseout", hide5)
+document.querySelector("#outer_7_").addEventListener("mouseout", hide6)
+document.querySelector("#outer_8_").addEventListener("mouseout", hide7)
+
+let waypoint = new waypoint({
+    element: document.querySelector('darkredCircle'),
+    handler: function(direction){
+        console.log('scrool to waypoint')
+    }
+})
+
